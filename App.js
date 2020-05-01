79f3f45d2ee5d05be2876500294655e4f0d596b0
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-05-01 22:12:59 +0300

    Added Todo. UseState support. Relations between input and todos list

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Navbar} from "./src/navbar";
 import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
 export default function App() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (title) => {
+        setTodos(prev => [
+            ...prev,
+            {
+                id: Date.now().toString(),
+                title: title
+            }]);
+    };
+
     return (
-        <View style={styles.container}>
-            <Navbar title="Todo App" />
-            <AddTodo />
+        <View>
+            <Navbar title="Todo App"/>
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo}/>
+
+                <View>
+                    {todos.map(todo => {
+                        return <Todo todo={todo} key={todo.id}/>
+                    })}
+                </View>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 20
+    }
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,15 +1,43 @@
-import React from 'react'
-import {View, StyleSheet, TextInput, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({onSubmit}) => {
+
+    const [value, setValue] = useState('');
+
+    const pressHandler = () => {
+        if (value.trim()) {
+            onSubmit(value);
+            setValue('');
+        } else {
+            Alert.alert('Дело не может быть пустым!');
+        }
+    };
+
     return (
-        <View styles={styles.block}>
-            <TextInput />
-            <Button title="Добавить"/>
+        <View style={styles.block}>
+            <TextInput
+                style={styles.input}
+                onChangeText={setValue}
+                value={value}
+                placeholder='Что хотите сделать?'
+            />
+            <Button title="Добавить" onPress={pressHandler}/>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    block: {}
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15
+    },
+    input: {
+        width: '70%',
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+    }
 });
