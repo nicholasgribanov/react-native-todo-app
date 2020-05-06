d4e5903a2b1d9704067221a488864861d7724155
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-05-06 19:40:07 +0300

    Style for buttons todo screen

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -5,12 +5,26 @@ export const TodoScreen = ({ todo, goBack }) => {
     return (
         <View>
             <Text>{todo.title}</Text>
-            <Button title="Назад" onPress={goBack} />
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title="Назад" onPress={goBack} color='#757575' />
+                </View>
+                <View style={styles.button}>
+                    <Button title='Удалить' onPress={() => console.log('remove')} color='#e53935' />
+                </View>
+            </View>
 
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    button: {
+        width: '40%'
+    }
 
 })
